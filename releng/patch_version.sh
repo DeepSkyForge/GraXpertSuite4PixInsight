8b7ee7ffeef3c3bd96d7d83832254d2a3767d684
AstroDeepSky <joel.vallier@gmail.com>
2023-12-21 23:36:41 +0100

    Update patch_version.sh

--- releng/patch_version.sh
+++ releng/patch_version.sh
@@ -9,10 +9,10 @@ commit=$(git log -1 --format='%H')
 tag=$(git tag -n --points-at $commit)
 IFS=' ' read -r version release <<< $tag
 
-line='#define VERSION "$version"'
+line='#define VERSION $version'
 if [[ $release != "" && $version != "" ]]; 
 then
-  sed -i -e '/^#define VERSION /c\#define VERSION \$version'  "$filename"
+  sed -i -e '/^#define VERSION /c\\$line'  "$filename"
 else
   echo "ERROR: Could not retrieve git release tag"
   exit 1
